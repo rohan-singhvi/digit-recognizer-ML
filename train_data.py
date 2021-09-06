8031df0237d398a4baed2d52dffbb872429a51c2
Rohan Singhvi <67252780+rohan-singhvi@users.noreply.github.com>
2021-09-05 22:50:55 -0400

    [#3] Creation of the Convolutional NN, and addition of the newly trained model (#3)
    * added a .h5 file for use in the GUI and removed the practice file that I had been working with in order to grasp how to use the MNIST data, and find out the structures that are used.
    
    * new use of convolutional neural network

--- train_data.py
+++ train_data.py
@@ -1,7 +1,11 @@
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.utils import np_utils
 import matplotlib.pyplot as plt
 
@@ -18,8 +22,8 @@ import matplotlib.pyplot as plt
 total_pixels = x_train.shape[1] * x_train.shape[2]  # both the training and testing data has the same sized images.
 # Use of 'float32' may seem odd, however this is to reduce the memory requirements
 # this is done by foring the precision of the pixel values to be 32 bit
-x_train = x_train.reshape((x_train.shape[0], total_pixels)).astype('float32')
-x_test = x_test.reshape((x_test.shape[0], total_pixels)).astype('float32')
+x_train = x_train.reshape((x_train.shape[0], 28, 28, 1)).astype('float32')
+x_test = x_test.reshape((x_test.shape[0], 28, 28, 1)).astype('float32')
 
 #after printing the train and test values, it was discovered that the inputs are values 0-255
 # inputs must be normalized, and since we know the max is 255, we can divide all values by 255 to get it between 0-1
@@ -38,15 +42,30 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 total_classes = y_test.shape[1]
 
-# creating a simple neural network model, defined in a function for ease of use
-def simple_model():
+# creating a 2d convolutional neural network model, defined in a function for ease of use
+def conv_model():
 	model = Sequential()
-	model.add(Dense(total_pixels, input_dim=total_pixels, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(total_classes, kernel_initializer='normal', activation='softmax'))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=(28, 28, 1)))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dropout(0.3))
+	model.add(Dense(64, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(10, activation='softmax'))
+
+	model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
 	return model
-model = simple_model()
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
+
+
+model = conv_model()
+trained_model = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=128, verbose=2)
+print("Training Successful")
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
 model.save('mnist.h5')
-scores = model.evaluate(x_test, y_test, verbose=0)
-print("Base error: %.2f%%" %(100 - scores[1]*100))
+print("Saving the model as mnist.h5")
